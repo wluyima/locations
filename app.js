6f033e95ff8e0a537cf4c50a72ac29e182aa36cd
Wyclif Luyima <wyclif.luyima@gmail.com>
2018-11-19 14:48:04 -0500

    Added stub implementations of the CRUD operations

--- app.js
+++ app.js
@@ -1,12 +1,38 @@
 var express = require('express');
-
-var app = express();
+var bodyParser = require('body-parser');
 
 var port = process.env.PORT || 3000;
+
+var app = express();
+var router = express.Router();
+app.use(bodyParser.json());
 
 app.get('/', function (req, res) {
     res.send("Welcome to the locations API");
 });
+
+router.route('').get(function(req, resp){
+    resp.send({list: 'This is the list of locations'});
+});
+
+router.route('/:id').get(function(req, resp){
+    resp.send({list: 'Location with id: '+req.params.id});
+});
+
+router.route('').post(function(req, resp){
+    console.log(req.body);
+    resp.send({list: 'Creating location with data: '+req.body});
+});
+
+router.route('/:id').put(function(req, resp){
+    resp.send({list: 'Updating location with id: '+req.params.id+', data = '+req.body});
+});
+
+router.route('/:id').delete(function(req, resp){
+    resp.send({list: 'Deleting location with id: '+req.params.id});
+});
+
+app.use('/api/locations', router);
 
 app.listen(port, function(){
     console.log('Running API on port '+port);
