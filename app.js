04c3c7ade32a126e1457af7ce6b22d623e9e252c
Wyclif Luyima <wyclif.luyima@gmail.com>
2018-11-27 12:15:35 -0500

    Added actual implementations of the CRUD operations and a database

--- app.js
+++ app.js
@@ -1,38 +1,95 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+const Sequelize = require('sequelize');
 
 var port = process.env.PORT || 3000;
+
+var sequelize = new Sequelize('locations', 'root', 'developer', {
+    dialect: 'mysql'
+});
+//var sequelize = new Sequelize('mysql://localhost:3306/locations');
+sequelize.authenticate()
+    .then(() => {
+        console.log('Connection has been established successfully.');
+    })
+    .catch(err => {
+        console.error('Unable to connect to the database:', err);
+    });
 
 var app = express();
 var router = express.Router();
 app.use(bodyParser.json());
+
+const Location = sequelize.define('location', {
+        id: {
+            type: Sequelize.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        address: Sequelize.STRING,
+        city: Sequelize.STRING,
+        state: Sequelize.STRING,
+        zipcode: {
+            type: Sequelize.INTEGER,
+            field: "zip_code"
+        }
+    }, {
+        freezeTableName: true,
+        underscored: true
+    });
+
+//Create the table
+Location.sync();
 
 app.get('/', function (req, res) {
     res.send("Welcome to the locations API");
 });
 
 router.route('').get(function(req, resp){
-    resp.json({list: 'This is the list of locations'});
+    Location.findAll().then((locations) => {
+        resp.json(locations);
+    });
 });
 
 router.route('/:id').get(function(req, resp){
-    resp.json({list: 'Location with id: '+req.params.id});
+    Location.findByPk(req.params.id).then((location) => {
+        resp.json(location);
+    });
 });
 
 router.route('').post(function(req, resp){
-    resp.json({list: 'Creating location with data: '+req.body});
+    //Or new Location(req.body).save().then(function(loc){});
+    Location.create(req.body).then((location) => {
+        resp.json(location);
+    });
 });
 
 router.route('/:id').put(function(req, resp){
-    resp.json({list: 'Updating location with id: '+req.params.id+', data = '+req.body});
+    Location.findByPk(req.params.id).then((location) => {
+        location.address = req.body.address;
+        location.city = req.body.city;
+        location.state = req.body.state;
+        location.zipcode = req.body.zipcode;
+        location.save().then(() => {
+            resp.json(location);
+        });
+    });
 });
 
 router.route('/:id').delete(function(req, resp){
-    resp.json({list: 'Deleting location with id: '+req.params.id});
+    //Or location.destroy().then(function(){});
+    //Or location.remove().then(function(){});
+    Location.destroy({
+        where: {
+            id: req.params.id
+        }
+    }).then(() => {
+        resp.status(204).end();
+    });
 });
 
 app.use('/api/locations', router);
 
 app.listen(port, function(){
-    console.log('Running API on port '+port);
+    console.log('Running Locations API on port '+port);
 });
